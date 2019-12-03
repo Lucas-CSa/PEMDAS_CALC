3f19786e6e6c11e436c099774885abc0a3bb74c2
Lucas-CSa <lucas.cisa19@gmail.com>
2019-12-02 23:58:21 -0300

    Error Fixedadd

--- PEMDAS.py
+++ PEMDAS.py
@@ -1,94 +1,151 @@
 def is_operand(var):
     return var in ('0123456789')
 
-def execute(nbrs):
-  if len(nbrs) < 2:
-    return nbrs
-  stack = []
-  a = 0
-  b = 0
-  r = 0
-  for i in nbrs:
-    if i == "+":
-      a = stack.pop()
-      b = stack.pop()
-      r = b + a
-      stack.append(r)
-    elif i == "-":
-      a = stack.pop()
-      b = stack.pop()
-      r = b - a
-      stack.append(r)
-    elif i == "*":
-      a = stack.pop()
-      b = stack.pop()
-      r = b * a
-      stack.append(r)
-    elif i == "/":
-      a = stack.pop()
-      b = stack.pop()
-      r = b / a
-      stack.append(r)
-    elif i == "^":
-      a = stack.pop()
-      b = stack.pop()
-      r = b ** a
-      stack.append(r)
-    else:
-      stack.append(i)
-  print(stack)
-  print(stack.pop())
+def validate(expression):
+    expression = expression.replace(" ", "")
+    expression = list(expression)
+    for element in expression:
+        if element not in ('0123456789/*+-^()'):
+            print("not here")
+            return False
+    return True
 
-def analyze(line):
-    line = line.replace(" ", "")
-    line += "z"
-    line = list(line)
+def operators(operation, num1, num2):
+    if operation == '+':
+        return num1 + num2
+    if operation == '-':
+        return num1-num2
+    if operation == '*':
+        return num1*num2
+    if operation == '/':
+        return num1/num2
+    if operation == '^':
+        return num1 ** num2
+
+def modArray(index, operations, numbers):
+    newNum = operators(operations[index], numbers[index], numbers[index+1])
+    del operations[index]
+    del numbers[index+1]
+    del numbers[index]
+    numbers.insert(index, newNum)
+    print(numbers)
+def calculator(numbers, operations, j):
+    for i in range(j):
+        if "^" in operations:
+            index = operations.index("^")
+            modArray(index, operations, numbers)
+        elif "*" in operations or "/" in operations:
+            mult = j
+            div = j
+            if "*" in operations:
+                mult = operations.index("*")
+            if "/" in operations:
+                div = operations.index("/")
+            if mult < div:
+                index = mult
+                modArray(index, operations, numbers)
+            else:
+                index = operations.index("/")
+                modArray(index, operations, numbers)
+        elif "+" in operations:
+            index = operations.index("+")
+            modArray(index, operations, numbers)
+        elif "-" in operations:
+            index = operations.index("-")
+            modArray(index, operations, numbers)
+    return numbers
+def analyze(numbers, expression):
     num = ""
-    nbrs = []
     op = ""
-    oprs = []
-    print(line)
-    print(nbrs)
-    print(oprs)
-    for i in range(len(line)):
-      if(is_operand(line[i])):
-        num += line[i]
-        print(num)
-      else:
-        if num != "":
-          nbrs.append(int(num))
-          num = ""
-        op = line[i]
-        oprs.append(op)
-        if len(oprs) > 1 and oprs[len(oprs) - 1] == "*":
-          print("check order: *")
-          if oprs[len(oprs) - 2] == "^" or oprs[len(oprs) - 2] == "/":
-            print('Wrong Order')
-            nbrs.append(oprs[len(oprs) - 2])
-            del oprs[len(oprs) - 2]
-        if len(oprs) > 1 and oprs[len(oprs) - 1] == "-" or oprs[len(oprs) - 1] == "+":
-          print("Check the Order")
-          if oprs[len(oprs) - 2] == "^" or oprs[len(oprs) - 2] == "/" or oprs[len(oprs) - 2] == "*":
-            print('Wrong Order')
-            nbrs.append(oprs[len(oprs) - 2])
-            del oprs[len(oprs) - 2]
-        if len(oprs) > 1 and oprs[len(oprs) - 1] == "/":
-          print("check order: /")
-          if oprs[len(oprs) - 2] == "^" or oprs[len(oprs) - 2] == "*":
-            print('Wrong Order')
-            nbrs.append(oprs[len(oprs) - 2])
-            del oprs[len(oprs) - 2]
-    oprs.pop()
-    print(nbrs)
-    print(oprs)
-    oprs = oprs[::-1]
-    for i in oprs:
-      nbrs.append(i)
-    print(nbrs)
-    execute(nbrs)
+    operations = []
+    result = []
 
+    expression = expression.replace(" ", "")
+    expression += "z"
+    expression = list(expression)
+    for i in range(len(expression)):
+        if(is_operand(expression[i])):
+            num += expression[i]
+        else:
+            numbers.append(int(num))
+            num = ""
+            op = expression[i]
+            operations.append(op)
+    operations.pop()
+    j = len(operations)
+    return calculator(numbers, operations, j)
 
+def calc(pairs, expression):
+    for i in range(len(pairs)):
+        openKey = pairs[i][0]
+        closeKey = pairs[i][1]
+        # print(openKey)
+        # print(closeKey)
+        innerOperation = expression[openKey+1:closeKey]
+        innerRes = analyze([],innerOperation)
+        expression = list(expression)
+        del expression[openKey]
+        expression.insert(openKey, str(innerRes[0]))
+        # print(expression)
+        cont = openKey+1
+        while cont <= closeKey:
+            del expression[cont]
+            expression.insert(cont, " ")
+            cont += 1
+        expression = "".join(expression)
+    return expression
+def pareth(expression):
+        iOpen = []
+        # contOpen = 0
+        iClose = []
+        # contClose = 0
+        pairs = []
+        arr = list(expression)
+        for i in range(len(arr)):
+            if arr[i] == "(":
+                iOpen.append(i)
+                # contOpen+=1
+            if arr[i] == ")":
+                iClose.append(i)
+                # contClose += 1
+        a=0
+        j=1
 
+        while a < len(iOpen)-1:
+            while j <len(iOpen):
+                if iOpen[j] > iClose[a]:
+                    pairs.append([iOpen[j-1],iClose[a]])
+                    del iOpen[j-1]
+                    del iClose[a]
+                j+=1
+            a += 1
 
-expression = input('Qual expressão a ser calculada: ')
-analyze(expression)
+        z =0
+        iClose.reverse()
+        while z < len(iOpen):
+            pairs.append([iOpen[z],iClose[z]])
+            del iOpen[z]
+            del iClose[z]
+
+        for j in range(len(pairs)):
+            for i in range(len(pairs)-j-1):
+                if (pairs[i][1] - pairs[i][0]) > (pairs[i+1][1] - pairs[i+1][0]):
+                    
+                    pairs[i], pairs[i+1] = pairs[i+1], pairs[i]
+
+        return calc(pairs, expression)
+
+expression = input("Digite a expressão a ser calculada: ")
+if validate(expression):
+    if ("(" in expression) & (")" in expression):
+        expression = "("+ expression + ")"
+        res = pareth(expression)
+        print("\n\n\n O resultado da operação é : " + res)
+
+    else:
+        numbers = []
+        analyze(numbers, expression)
+        print(numbers[0])
+else:
+    print("error")
+
